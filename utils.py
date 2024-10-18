9a352f56e4f1bc42b92d3965550ebf94b8e39338
John <126716048+JoJonatasCoelho@users.noreply.github.com>
2024-10-18 15:23:32 -0300

    added the client class

--- utils.py
+++ utils.py
@@ -11,3 +11,16 @@ class Saldo:
 
     def getter(self):
         return self.__saldo
+
+class Cliente:
+    def __init__(self, *kwargs):
+        self.__nome = nome
+        self.__email = email
+        self.__senha = senha
+
+    def get_nome(self, id):
+        return self.__nome
+
+    def get_email(self, email):
+        return self.__email
+
