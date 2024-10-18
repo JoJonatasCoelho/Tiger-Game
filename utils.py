d48f174525d779cc43e39500a9f992608aace48b
John <126716048+JoJonatasCoelho@users.noreply.github.com>
2024-10-18 12:05:45 -0300

    Create Saldo class

--- utils.py
+++ utils.py
@@ -1,3 +1,13 @@
 def ler_dinheiro():
     dinheiro = int(input("Digite a quantidade de dinheiro necessária: R$"))
     return dinheiro
+
+class Saldo:
+    def __init__(self, saldo):
+        self.__saldo = saldo
+
+    def setter(self, nsaldo):
+        self.__saldo = nsaldo
+
+    def getter(self):
+        return self.saldo
